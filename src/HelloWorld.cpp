33c669b478e31b727fa3f0d42d50dda8e4a22811
Victor <vialexan@ryerson.ca>
2015-10-23 16:46:15 -0400

    ddd

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Victor
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "tttttttttttttt" << endl; // prints !!!Hello World!!!
+	int x = 5;
+
+	return 0;
+}
